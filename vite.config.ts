83b4e7b007cab4e40fe59651beb70c37b23d7c1d
stanislavcumakov <3apa3a.iickov@gmail.com>
2026-02-27 03:06:45 +0300

    add baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    base: '/',
+    base: '/spacedev-cli/',
     plugins: [
       vue(),
       mode === 'development' && vueDevTools(),
